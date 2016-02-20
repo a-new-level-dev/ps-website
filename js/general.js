c7dbd573d02e5b00837601e63085b6833112afb7
Gabriel Lujan <contact@anewlevelmedia.com>
2016-02-19 22:20:56 -0800

    adding my commits to the creatingStaging branch to then be merged with the master

--- js/general.js
+++
@@ -1,9 +0,0 @@
-// This is a simple JavaScript comment for example purposes
-function copyright() {
-	var date = new Date();
-	var year = date.getUTCFullYear();
-
-	document.getElementById('copyright').innerHTML = year;
-}
-
-copyright();
---
+++ scripts/js/portfolios.js
@@ -0,0 +1,3 @@
+// This is a simple JavaScript statement for example purposes
+
+// http://www.emanueleferonato.com/2007/08/22/create-a-lightbox-effect-only-with-css-no-javascript-needed/
