56e6390971718d035d678c321eb190c9c0e61907
Gabriel Lujan <contact@anewlevelmedia.com>
2016-04-11 12:17:46 -0700

    more updates for the staging area

--- scripts/php/base-url.php
+++ scripts/php/base-url.php
@@ -1,4 +1,7 @@
 <?php
-	// Setting the base URL for all pages on the site
-	define('BASE_URL', 'http://staging.gflujan.com');
+	//Setting the base STAGING URL for all pages on the site
+	define('BASE_URL', 'http://staging.gflujan.com/painted-siren');
+
+	//Setting the base PRODUCTION URL for all pages on the site
+	//define('BASE_URL', 'http://paintedsiren.com');
 ?>
